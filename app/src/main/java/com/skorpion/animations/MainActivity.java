6216d0a1b7bac1e1e6d8430f301556b62077e73f
Rohit Dubey <dubey22rohit@gmail.com>
2020-02-08 10:10:39 +0530

    part 1 done!!!!

--- app/src/main/java/com/skorpion/animations/MainActivity.java
+++ app/src/main/java/com/skorpion/animations/MainActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 public TextView txtHelloWorld;
 public TextView txtHiWorld;
-public boolean isHelloWorldShowing;
+public TextView txtApp;
+public Button txtButton;
+public boolean isHelloWorldShowing = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +21,12 @@ public boolean isHelloWorldShowing;
 
         txtHelloWorld = findViewById(R.id.txtHelloWorld);
         txtHiWorld = findViewById(R.id.txtHiWorld);
+        txtApp = findViewById(R.id.txtApp);
+        txtButton = findViewById(R.id.txtButton);
+
+        txtHelloWorld.setX(2000);
+        txtHiWorld.setX(2000);
+        txtApp.setY(2000);
 
 
 
@@ -37,6 +46,24 @@ public boolean isHelloWorldShowing;
                 }
             }
         });
+
+        txtApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtApp.animate().translationXBy(1200).setDuration(1000);
+
+            }
+        });
+        txtButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtHelloWorld.animate().translationXBy(-2000).rotation(45).setDuration(1200);
+                txtHiWorld.animate().translationXBy(-2500).rotation(-45).setDuration(2000);
+                txtApp.animate().translationYBy(-2000).rotation(25).setDuration(1000);
+
+            }
+        });
+
 
 
 
